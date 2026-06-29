179fe2757bf5e6056e7e0e5b7bd5459dad8548f0
vajrabilashdegapogu <vajrabilashd@gmail.com>
2026-06-29 21:57:47 +0530

    updates

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const api = axios.create({
   baseURL:
-    process.env.REACT_APP_API_URL || "https://api.owntag.in/api",
+    "http://localhost:3000" || "https://api.owntag.in/api",
   timeout: 15000,
   headers: {
     "Content-Type": "application/json"
